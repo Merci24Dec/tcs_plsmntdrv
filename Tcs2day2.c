b01c119302fde1f51d196cc990f3fc0a25b46f70
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-03-31 06:38:31 +0530

    Add files via upload

---
+++ Tcs2day2.c
@@ -0,0 +1,27 @@
+﻿//a program in c to check whether the number is even or odd without using modulous operator
+//testcase1,2: input:6              input:99           
+//               output:even        output:odd  
+
+#include<stdio.h>
+int main()
+{
+  int num;
+    scanf("%d", &num);
+      int half = num/2;
+  if( num == half*2 )
+      printf("Even");
+        else
+            printf("Odd");
+  return 0;
+  }
+
+
+OR
+#include<stdio.h>
+int main()
+{
+  int num=9;
+  float res= num/2.0;
+  printf("%.2f",res);
+  return 0;
+}
