6cc46836669a172955098e97d7d3e04f5970a92c
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-03-31 06:38:09 +0530

    Add files via upload

---
+++ tcs1_day2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main() 
+{
+    int n, i, flag = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    for (i = 2; i <= n / 2; ++i) 
+       {
+
+        if (n % i == 0) 
+          {
+            flag++;
+            break;
+        }
+          else 
+         {
+        if (flag == 2)
+          {
+            printf("%d",sq);
+}
+        else if(n%2==0)
+            {
+            printf("%f",d);
+    }
+
+    return 0;
+}
+}
+}
