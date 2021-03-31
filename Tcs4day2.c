15b113f36242880dd4ba02c0e1cf7241b4a46189
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-03-31 06:41:01 +0530

    Add files via upload

---
+++ Tcs4day2.c
@@ -0,0 +1,11 @@
+//a program in c to conert decimal to hexadecimal 
+//testcase1,2: input:99             input:79          
+//               output:63            output:4f       
+#include<stdio.h>
+ int main() 
+  {
+      int num;
+          scanf("%d", &num);
+              printf(" %x ", num);
+                  return 0;
+                   }
