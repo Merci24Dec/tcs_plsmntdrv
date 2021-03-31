50d102e5ead273b1b0feb85073c9dc503e614ef8
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-03-31 06:38:52 +0530

    Add files via upload

---
+++ Tcs3day2.c
@@ -0,0 +1,11 @@
+//a program in c to print ASCII value of a character 
+//testcase1,2: input:A              input:a         
+//               output:65            output:97   
+#include<stdio.h>
+ int main() 
+  {
+      char c;
+          scanf(" %c ", &c);
+              printf(" %d ", c);
+                  return 0;
+                   }
