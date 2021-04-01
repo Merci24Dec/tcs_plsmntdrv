5a5a6a61437a3b0bf04e2d81642d5fe22517208f
Akash Dubey <80431506+Merci24Dec@users.noreply.github.com>
2021-04-01 05:34:52 +0530

    Add files via upload

---
+++ tcs9day3.c
@@ -0,0 +1,15 @@
+//a program in c to swap two numbers without third vriable.  
+//testcase1,2: input:10 20             input:15 22        
+//               output:20 10            output:22 15        
+int printf(const char *format, ...);
+int scanf(const char *format, ...);
+int main()
+{
+int a,b;
+scanf("%d%d",&a,&b);
+a=a-b;
+b=a+b;
+a=b-c;
+printf("%d%d",A,B);
+return 0;
+}
